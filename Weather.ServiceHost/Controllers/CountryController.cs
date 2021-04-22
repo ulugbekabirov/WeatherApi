d6f773162efed7976a0e676b81c76c77c73c2b56
ulugbekabirov@gmail.com <ulugbekabirov@gmail.com>
2021-04-22 12:57:46 +0500

    added city and country controllers

--- Weather.ServiceHost/Controllers/CountryController.cs
+++ Weather.ServiceHost/Controllers/CountryController.cs
@@ -4,35 +4,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace Weather.ServiceHost.Controllers
 {
-    [Route("api/city")]
-    public class CityController : ControllerBase
+    [Route("api/[controller]")]
+    public class CountryController : ControllerBase
     {
         [HttpGet]
-        public async Task<IEnumerable<string>> GetCities()
+        public async Task<IEnumerable<string>> GetCountries()
         {
             return new string[] { "value1", "value2" };
         }
 
         [HttpGet("{id}")]
-        public async Task<string> GetCityById(int id)
+        public async Task<string> GetCountryById(int id)
         {
             return "value";
         }
 
         [HttpPost]
-        public async Task CreateCity([FromBody] string value)
+        public async Task CreateCountry([FromBody] string value)
         {
         }
 
         [HttpPut("{id}")]
-        public async Task UpdateCity(int id, [FromBody] string value)
+        public async Task UpdateCountry(int id, [FromBody] string value)
         {
         }
 
         [HttpDelete("{id}")]
-        public async Task DeleteCity(int id)
+        public async Task DeleteCountry(int id)
         {
         }
     }
