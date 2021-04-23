6ab5ba49bb589fd0be0bba840cccf2a180dab68e
ulugbekabirov@gmail.com <ulugbekabirov@gmail.com>
2021-04-23 17:59:38 +0500

    added interfaces for repositoties

---
+++ Weather.RA/Interfaces/ICityRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Weather.Data.Entities;
+
+namespace Weather.RA.Interfaces
+{
+    public interface ICityRepository : IRepository<City>
+    {
+    }
+}
---
+++ Weather.RA/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.RA.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+    }
+}
