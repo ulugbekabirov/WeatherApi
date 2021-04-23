841f5c780d2da649851dfb32f756178171e2f514
ulugbekabirov@gmail.com <ulugbekabirov@gmail.com>
2021-04-23 17:59:48 +0500

    added repositories

---
+++ Weather.RA/SqlRepositories/CountryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Weather.Data.Entities;
+using Weather.RA.Interfaces;
+
+namespace Weather.RA.SqlRepositories
+{
+    public class CountryRepository : ICountryRepository
+    {
+    }
+}
