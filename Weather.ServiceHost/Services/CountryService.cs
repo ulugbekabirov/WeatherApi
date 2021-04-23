fb14ba206186a6818a1f2ca34af9a86517f1aec8
ulugbekabirov@gmail.com <ulugbekabirov@gmail.com>
2021-04-23 18:00:30 +0500

    added servisces

---
+++ Weather.ServiceHost/Services/CountryService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Weather.SDK.Interfaces;
+
+namespace Weather.ServiceHost.Services
+{
+    public class CountryService: ICountryService
+    {
+    }
+}
