a2e872093406e7759c1c90ec33c0b2b56eb419ba
ulugbekabirov@gmail.com <ulugbekabirov@gmail.com>
2021-04-23 14:19:51 +0500

    made country entity

---
+++ Weather.Data/Entities/Country.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.Data.Entities
+{
+    public class Country
+    {
+        public Country()
+        {
+            Cities = new List<City>();
+        }
+
+        [Required]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(2)]
+        public string AlphaCode { get; set; }
+
+        public ICollection<City> Cities { get; set; }
+    }
+}
