54304106f571b3837a87ae90cd5368fca832c204
ulugbekabirov@gmail.com <ulugbekabirov@gmail.com>
2021-04-23 14:40:09 +0500

    added context to startup

---
+++ Weather.RA/DbContexts/SQLContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Weather.Data.Entities;
+
+namespace Weather.RA.DbContexts
+{
+    public class SQLContext : DbContext
+    {
+        public SQLContext() : base()
+        {
+            
+        }
+
+        public DbSet<City> Cities { get; set; }
+
+        public DbSet<Country> Countries { get; set; }
+    }
+}
--- Weather.ServiceHost/Startup.cs
+++ Weather.ServiceHost/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Weather.RA.DbContexts;
 
 namespace Weather.ServiceHost
 {
@@ -26,12 +28,13 @@ namespace Weather.ServiceHost
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Weather.ServiceHost", Version = "v1" });
             });
+
+            services.AddDbContext<SQLContext>(item => item.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
