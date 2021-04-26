ddd5394284380927ea49273924495b617c91868c
ulugbekabirov@gmail.com <ulugbekabirov@gmail.com>
2021-04-26 16:39:37 +0500

    changed services

--- Weather.ServiceHost/Services/CountryService.cs
+++ Weather.ServiceHost/Services/CountryService.cs
@@ -1,8 +1,18 @@
-﻿using Weather.SDK.Interfaces;
+﻿using AutoMapper;
+using MediatR;
+using Weather.SDK.Interfaces;
 
 namespace Weather.ServiceHost.Services
 {
     public class CountryService: ICountryService
     {
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
+
+        public CountryService(IMapper mapper, IMediator mediator)
+        {
+            _mapper = mapper;
+            _mediator = mediator;
+        }
     }
 }
