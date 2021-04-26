9e639d7dbcc7a3a3182f0c969e043867209242a2
ulugbekabirov@gmail.com <ulugbekabirov@gmail.com>
2021-04-26 16:39:18 +0500

    added commands

---
+++ Weather.ServiceHost/Commands/CreateCityCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Weather.Data.Entities;
+
+namespace Weather.ServiceHost.Commands
+{
+    public class CreateCityCommand: IRequest
+    {
+        public City City { get; set; }
+    }
+}
---
+++ Weather.ServiceHost/Commands/CreateCountryCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Weather.Data.Entities;
+
+namespace Weather.ServiceHost.Commands
+{
+    public class CreateCountryCommand: IRequest
+    {
+        public Country Country { get; set; }
+    }
+}
