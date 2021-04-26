a8974a9c19280f10f3f61f64a51791d012790065
ulugbekabirov@gmail.com <ulugbekabirov@gmail.com>
2021-04-26 18:28:19 +0500

    implemented country controller

--- Weather.ServiceHost/Controllers/CountryController.cs
+++ Weather.ServiceHost/Controllers/CountryController.cs
@@ -1,38 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-
+using Weather.SDK.DTO;
+using Weather.ServiceHost.Commands.CountryCommands;
 
 namespace Weather.ServiceHost.Controllers
 {
     [Route("api/[controller]")]
     public class CountryController : ControllerBase
     {
-        [HttpGet]
-        public async Task<IEnumerable<string>> GetCountries()
+        private readonly IMediator _mediator;
+
+        public CountryController(IMediator mediator)
         {
-            return new string[] { "value1", "value2" };
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<CountryDTO>> GetCountries()
+        {
+            return await _mediator.Send(new GetAllCountriesCommand());
         }
 
         [HttpGet("{id}")]
-        public async Task<string> GetCountryById(int id)
+        public async Task<CountryDTO> GetCountryById(int id)
         {
-            return "value";
+            return await _mediator.Send(new GetCountryCommand() { CountryId = id });
         }
 
         [HttpPost]
-        public async Task CreateCountry([FromBody] string value)
+        public async Task CreateCountry([FromBody] CountryDTO country)
         {
+            await _mediator.Send(new CreateCountryCommand() { Country = country });
         }
 
         [HttpPut("{id}")]
-        public async Task UpdateCountry(int id, [FromBody] string value)
+        public async Task UpdateCountry(int id, [FromBody] CountryDTO country)
         {
+            await _mediator.Send(new UpdateCountryCommand() { Country = country });
         }
 
         [HttpDelete("{id}")]
         public async Task DeleteCountry(int id)
         {
+            await _mediator.Send(new DeleteCountryCommand() { CountryId = id });
         }
     }
 }
