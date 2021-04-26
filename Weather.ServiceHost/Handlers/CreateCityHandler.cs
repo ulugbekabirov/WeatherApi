a78a4d9a3cff23b1b6b652189a021be0201ff282
ulugbekabirov@gmail.com <ulugbekabirov@gmail.com>
2021-04-26 16:39:27 +0500

    created handlers

---
+++ Weather.ServiceHost/Handlers/CreateCityHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using Weather.RA.SqlRepositories;
+using Weather.ServiceHost.Commands;
+
+namespace Weather.ServiceHost.Handlers
+{
+    public class CreateCityHandler : AsyncRequestHandler<CreateCityCommand>
+    {
+        private readonly CityRepository _cityRepository;
+
+        protected override async Task Handle(CreateCityCommand request, CancellationToken cancellationToken)
+        {
+            await _cityRepository.CreateAsync(request.City);
+        }
+    }
+}
---
+++ Weather.ServiceHost/Handlers/CreateCountryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using Weather.Data.Entities;
+using Weather.RA.SqlRepositories;
+using Weather.ServiceHost.Commands;
+
+namespace Weather.ServiceHost.Handlers
+{
+    public class CreateCountryHandler : AsyncRequestHandler<CreateCountryCommand>
+    {
+        private readonly CountryRepository _countryRepository;
+
+        public CreateCountryHandler(CountryRepository countryRepository)
+        {
+            _countryRepository = countryRepository;
+        }
+
+        protected override async Task Handle(CreateCountryCommand request, CancellationToken cancellationToken)
+        {
+            await _countryRepository.CreateAsync(request.Country);
+        }
+    }
+}
