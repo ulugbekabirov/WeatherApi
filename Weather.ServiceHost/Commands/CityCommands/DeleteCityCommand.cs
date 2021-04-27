5bfd26f7613dd329958345561cc1e093b9c302af
ulugbekabirov@gmail.com <ulugbekabirov@gmail.com>
2021-04-27 13:31:57 +0500

    deleted commands

--- Weather.ServiceHost/Commands/CityCommands/DeleteCityCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace Weather.ServiceHost.Commands.CityCommands
-{
-    public class DeleteCityCommand: IRequest
-    {
-        public int CityId { get; set; }
-    }
-}
--- Weather.ServiceHost/Commands/CountryCommands/DeleteCountryCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace Weather.ServiceHost.Commands.CountryCommands
-{
-    public class DeleteCountryCommand : IRequest
-    {
-        public int CountryId { get; set; }
-    }
-}
