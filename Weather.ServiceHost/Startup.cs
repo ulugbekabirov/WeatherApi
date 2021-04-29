173fbae1453c830898efbc5fd949b8c1cbc68e73
ulugbekabirov@gmail.com <ulugbekabirov@gmail.com>
2021-04-29 10:42:03 +0500

    migrated

--- Weather.ServiceHost/Startup.cs
+++ Weather.ServiceHost/Startup.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -8,9 +9,6 @@ using Microsoft.OpenApi.Models;
 using Weather.RA.DbContexts;
 using Weather.RA.Interfaces;
 using Weather.RA.SqlRepositories;
-using Weather.ServiceHost.Services;
-using Weather.SDK.Interfaces;
-using MediatR;
 using Weather.ServiceHost.Mappings;
 
 namespace Weather.ServiceHost
@@ -33,13 +31,10 @@ namespace Weather.ServiceHost
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Weather.ServiceHost", Version = "v1" });
             });
 
-            services.AddDbContext<SqlContext>(item => item.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<SqlContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddScoped<ICityRepository, CityRepository>();
             services.AddScoped<ICountryRepository, CountryRepository>();
-
-            services.AddScoped<ICityService, CityService>();
-            services.AddScoped<ICountryService, CountryService>();
 
             services.AddMediatR(typeof(Startup));
 
