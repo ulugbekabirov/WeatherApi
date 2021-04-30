a4b0eb79f6a2fb6a26f5aa6ece14eac062598fbd
ulugbekabirov@gmail.com <ulugbekabirov@gmail.com>
2021-04-30 15:13:31 +0500

    implemented city validation

---
+++ Weather.ServiceHost/Validators/CityValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Weather.Data.Entities;
+
+namespace Weather.ServiceHost.Validators
+{
+    public class CityValidator : AbstractValidator<City>
+    {
+        public CityValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty().WithMessage("City name is required");
+            RuleFor(c => c.CountryId).NotEmpty().WithMessage("Country id is required");
+        }
+    }
+}
