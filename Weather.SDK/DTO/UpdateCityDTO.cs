5f21f8d11fb753e04ece01ccdefb1b09d145397f
ulugbekabirov@gmail.com <ulugbekabirov@gmail.com>
2021-04-30 15:46:27 +0500

    created updatedto

---
+++ Weather.SDK/DTO/UpdateCityDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Weather.SDK.DTO
+{
+    public class UpdateCityDTO
+    {
+        public Guid CountryId { get; set; }
+
+        public string Name { get; set; }
+
+        public double Version { get; set; }
+    }
+}
---
+++ Weather.SDK/DTO/UpdateCountryDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.SDK.DTO
+{
+    public class UpdateCountryDTO
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string AlphaCode { get; set; }
+
+        public double Version { get; set; }
+    }
+}
