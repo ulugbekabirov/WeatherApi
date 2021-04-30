d751abf6c55966a5a6f4c2b36799903b34c73148
ulugbekabirov@gmail.com <ulugbekabirov@gmail.com>
2021-04-30 15:13:49 +0500

    alha code length validator

---
+++ Weather.ServiceHost/Validators/CountryValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using Weather.Data.Entities;
+
+namespace Weather.ServiceHost.Validators
+{
+    public class CountryValidator : AbstractValidator<Country>
+    {
+        private const int MinAlphaCodeLength = 2;
+        private const int MaxAlphaCodeLength = 3;
+
+        public CountryValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty().WithMessage("Country name is required");
+            RuleFor(c => c.AlphaCode).NotEmpty().WithMessage("AlphaCode of country is required");
+            RuleFor(c => c.AlphaCode).Length(MinAlphaCodeLength, MaxAlphaCodeLength).WithMessage("AlphaCode length is either 2 or 3");
+        }
+    }
+}
