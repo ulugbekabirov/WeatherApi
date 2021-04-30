23ea53de6fa4116293de8a70503b0a5424cfe05a
ulugbekabirov@gmail.com <ulugbekabirov@gmail.com>
2021-04-30 12:54:45 +0500

    nchar to varchar

--- Weather.Database/dbo/Tables/Countries.sql
+++ Weather.Database/dbo/Tables/Countries.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Countries] (
     [Id]        UNIQUEIDENTIFIER           NOT NULL,
     [Name]      VARCHAR (255) NOT NULL,
-    [AlphaCode] NCHAR (10)    NOT NULL,
+    [AlphaCode] VARCHAR(50)    NOT NULL,
     [Version]   INT           NOT NULL,
     [IsDeleted] BIT           NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
