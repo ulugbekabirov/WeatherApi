1ca1c674b2e70ab75076213fdc9d63a53d221e9a
Ulugbek Abirov <ulugbekabirov@gmail.com>
2021-05-05 18:00:35 +0500

    added mongo context

---
+++ Weather.RA/DbContexts/MongoDBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+using Weather.Data.Entities;
+
+namespace Weather.RA.DbContexts
+{
+    public class MongoDBContext
+    {
+        private readonly IMongoDatabase _context;
+
+        public MongoDBContext(string database)
+        {
+            var client = new MongoClient();
+            _context = client.GetDatabase(database);
+        }
+    }
+}
---
+++ Weather.ServiceHost/Controllers/WeatherApiController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Weather.ServiceHost.Controllers
+{
+    [Route("api/[]")]
+    [ApiController]
+    public class WeatherApiController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public WeatherApiController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCountries()
+        {
+            return;
+        }
+    }
+}
